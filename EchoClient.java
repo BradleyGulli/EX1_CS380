fc943fb5e4c5e4612b413f3d27b4533f1c38c41e
Bradley Gulli <bjgulli@cpp.edu>
2017-03-31 23:13:25 -0700

    EchoServer and EchoClient working.

--- EchoClient.java
+++ EchoClient.java
@@ -3,6 +3,8 @@ import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 public final class EchoClient {
 
@@ -12,6 +14,28 @@ public final class EchoClient {
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             System.out.println(br.readLine());
+            
+            OutputStream os = socket.getOutputStream();
+            PrintStream out = new PrintStream(os, true, "UTF-8");
+            
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            String inLine = "";
+            String ret = "";
+            while(!inLine.equals("exit")){
+            	System.out.print("Client> ");
+            	inLine = in.readLine();
+            	out.println(inLine);
+            	if(!(ret = br.readLine()).equals("exit")){
+            		System.out.print("Server> ");
+            		System.out.println(ret);
+            	}
+            	
+            }
+            
+            
+           
+            
         }
     }
 }
--- EchoServer.java
+++ EchoServer.java
@@ -3,6 +3,9 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public final class EchoServer {
 
@@ -15,6 +18,21 @@ public final class EchoServer {
                     OutputStream os = socket.getOutputStream();
                     PrintStream out = new PrintStream(os, true, "UTF-8");
                     out.printf("Hi %s, thanks for connecting!%n", address);
+                    
+                    InputStream is = socket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                    BufferedReader br = new BufferedReader(isr);
+                    String inLine = br.readLine();
+                    while(inLine != null){
+                    	out.println(inLine);
+                    	inLine = br.readLine();
+                    }
+                    
+                    
+                    System.out.println("Client disconnected: " + address);
+                    socket.close();
+                    
+                    
                 }
             }
         }
